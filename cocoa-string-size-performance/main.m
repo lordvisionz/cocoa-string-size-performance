3fe1dbd5add81b4f345b43b12081087346f07260
Abhishek Moothedath <lord.visionz@gmail.com>
2015-06-01 11:43:56 -0400

    added new method to reuse NSTextStorage objects.

--- cocoa-string-size-performance/main.m
+++ cocoa-string-size-performance/main.m
@@ -9,6 +9,7 @@
 #import <Cocoa/Cocoa.h>
 
 #define ITEMS_COUNT 1000000
+#define ITEMS_ARRAY_CHUNK_THRESHOLD 10000
 
 NS_INLINE void performAnalysisUsingSizeWithAttributes(NSArray* items)
 {
@@ -50,9 +51,11 @@ NS_INLINE void performAnalysisUsingNSLayoutManager(NSArray* items)
     NSMutableArray *textStorageObjects = [NSMutableArray new];
     for(NSString *item in items)
     {
-        NSTextStorage *textStorage = [[NSTextStorage alloc] initWithString:item];
-        [textStorage addLayoutManager:layoutManager];
-        [textStorageObjects addObject:textStorage];
+        @autoreleasepool {
+            NSTextStorage *textStorage = [[NSTextStorage alloc] initWithString:item];
+            [textStorage addLayoutManager:layoutManager];
+            [textStorageObjects addObject:textStorage];
+        }
     }
     timeTaken = -[startDate timeIntervalSinceNow];
     NSLog(@"-----------------------Using NSLayoutManager------------------\n");
@@ -63,6 +66,47 @@ NS_INLINE void performAnalysisUsingNSLayoutManager(NSArray* items)
     width = [layoutManager usedRectForTextContainer:textContainer].size.width;
     timeTaken = -[startDate timeIntervalSinceNow];
     NSLog(@"Time taken to measure the width(%f) of %li strings is %f\n\n\n",width, items.count, timeTaken);
+}
+
+NS_INLINE void performAnalysisByChunkingAndReusingLayoutManagerObjects(NSArray* items)
+{
+    NSMutableArray *chunks = [NSMutableArray new];
+
+    for(NSUInteger i = 0; i < items.count; i++)
+    {
+        NSUInteger chunkIndex = i / ITEMS_ARRAY_CHUNK_THRESHOLD;
+        if(chunks.count <= chunkIndex)
+        {
+            NSMutableArray *chunk = [NSMutableArray new];
+            [chunks addObject:chunk];
+        }
+        NSMutableArray *chunk = [chunks objectAtIndex:chunkIndex];
+        [chunk addObject:[items objectAtIndex:i]];
+    }
+    
+    double width = 0, timeTaken = 0;
+    NSDate *startDate = [NSDate dateWithTimeIntervalSinceNow:0];
+    
+    NSLayoutManager *layoutManager = [NSLayoutManager new];
+    NSTextContainer *textContainer = [NSTextContainer new];
+    NSTextStorage *textStorage = [NSTextStorage new];
+    [textStorage addLayoutManager:layoutManager];
+    
+    [textContainer setLineFragmentPadding:0];
+    [layoutManager addTextContainer:textContainer];
+    
+    for(NSArray *chunk in chunks)
+    {
+        for(NSString *item in chunk)
+        {
+            [textStorage replaceCharactersInRange:NSMakeRange(0, textStorage.length) withString:item];
+            [layoutManager glyphRangeForTextContainer:textContainer];
+            width = MAX(width,[layoutManager usedRectForTextContainer:textContainer].size.width);
+        }
+    }
+    timeTaken = -[startDate timeIntervalSinceNow];
+    NSLog(@"-----------------------Reusing NSTextStorage and chunking ------------------\n");
+    NSLog(@"Time taken to create %li NSTextStorage objects and measure them is %f",items.count, timeTaken);
 }
 
 int main(int argc, const char * argv[])
@@ -79,6 +123,7 @@ int main(int argc, const char * argv[])
     performAnalysisUsingSizeWithAttributes(items);
     performAnalysisUsingNSAttributedString(items);
     performAnalysisUsingNSLayoutManager(items);
+    performAnalysisByChunkingAndReusingLayoutManagerObjects(items);
     
     return 0;
 }
